c1d24f2a95b63ea780c3c9c0ae6cf014f9569501
Michael Siehl <miesiehl@gmail.com>
2016-11-27 13:35:01 +0100

    Add files via upload

---
+++ 161127_src/Main.f90
@@ -0,0 +1,10 @@
+! https://github.com/MichaelSiehl/Using-Atomic-Subroutines-and-Sync-Memory-to-Restore-Segment-Ordering
+! src_161123
+
+program Main
+  !
+  use Main_Sub
+  !
+  call Entry_Main_Sub ()
+  !
+end program Main
